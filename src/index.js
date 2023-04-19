036f4e91495e2c9d839fc90d489b65ed671fe741
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-04-18 22:53:56 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener("submit", (e) => {
+    e.preventDefault();
+    let b = document.getElementById('new-task-description');
+    todoList(b.value);
+  })
 });
+
+function todoList(todo){
+  let p = document.createElement("p");
+  p.textContent = todo;
+  document.getElementById('tasks').appendChild(p);
+  
+}
